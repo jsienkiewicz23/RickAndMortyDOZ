e7da7f88c0d40b7f0b6a3a55a77d3662d2449257
jsienkiewicz23 <joannasienkiewicz23@gmail.com>
2023-08-31 22:32:33 +0200

    Initial commit

---
+++ src/main/java/com/jsi/rickandmortydoz/RickAndMortyDozApplication.java
@@ -0,0 +1,13 @@
+package com.jsi.rickandmortydoz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickAndMortyDozApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RickAndMortyDozApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jsi/rickandmortydoz/RickAndMortyDozApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsi.rickandmortydoz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickAndMortyDozApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
